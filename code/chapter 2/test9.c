6ffabc72a80ad801fc6a30d07d53bf97542bc179
lincode2005 <lincode2005@gmail.com>
2025-11-14 13:38:14 +0800

    初版

---
+++ code/chapter 2/test9.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+int main()
+{
+    float h, r, pi, v;
+    h = 3;
+    r = 1.5;
+    pi = 3.1415926;
+    v = pi*r*r*h;
+    printf("Ô²ÖùµÄÌå»ıV = %f\n",v);
+    return 0;
+}
+
