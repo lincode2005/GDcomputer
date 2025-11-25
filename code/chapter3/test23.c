16406b9b73b980ed72918af831346b5cb8b3352a
lincode2005 <lincode2005@gmail.com>
2025-11-25 17:32:04 +0800

    初版

---
+++ code/chapter3/test23.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    putchar('A');
+    putchar('\n');
+    putchar(97);
+    return 0;
+}
+
--- code/test/demo1.c
+++ code/test/demo1.c
@@ -1,13 +1,31 @@
 #include <stdio.h>
 
-
-int main()
-{
-   float a;
-   scanf("%3f", &a);  // 非法的输入方式
-   printf("%f", a);
-   return 0;
+int main() {
+    int rows = 10;
+    int triangle[10][10];
+    int i, j;
+    
+    // 初始化杨辉三角形
+    for(i = 0; i < rows; i++) {
+        // 每行的第一个和最后一个元素都是1
+        triangle[i][0] = 1;
+        triangle[i][i] = 1;
+        
+        // 计算中间的元素
+        for(j = 1; j < i; j++) {
+            triangle[i][j] = triangle[i-1][j-1] + triangle[i-1][j];
+        }
+    }
+    
+    // 打印杨辉三角形（左对齐）
+    for(i = 0; i < rows; i++) {
+        // 打印数字
+        for(j = 0; j <= i; j++) {
+            printf("%d\t", triangle[i][j]);
+        }
+        printf("\n");
+    }
+    
+    return 0;
 }
-
-
 
