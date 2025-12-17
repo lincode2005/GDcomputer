419f49236fb59fb43140e762536f46da851ced21
lincode2005 <lincode2005@gmail.com>
2025-12-17 13:04:19 +0800

    初版

---
+++ code/chapter6/demo17.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    char c[5] = {'a', 'b', 'c', 'd', 'e'};
+    int i;
+    for(i=0; i<5; i++)
+        putchar(c[i]);
+    return 0;
+}
+
---
+++ code/chapter6/demo18.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    char c[] = {"abc"};  // ¿ª±Ù4¸ö×Ö½Ú
+    int i;
+    for(i=0; i<3; i++)
+        putchar(c[i]);
+    return 0;
+}
+
+
